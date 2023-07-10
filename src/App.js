bbe47a57fa2eb6592295ccbe239f640a5a809f26
karthigayani <karthi1826@gmail.com>
2023-07-10 22:33:48 +0530

    sm-frontend

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import { Routes, Route, Link, Navigate } from "react-router-dom";
+import HomePage from "./scenes/homePage/index";
+import LoginPage from "./scenes/loginPage/index";
+import ProfilePage from "./scenes/profilePage/index";
+import { useMemo } from "react";
+import { useSelector } from "react-redux";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { createTheme } from "@mui/material/styles";
+import { themeSettings } from "./theme";
+
+function App() {
+
+  const mode = useSelector((state) => state.mode);
+  const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
+  const isAuth = Boolean(useSelector((state) => state.token));
+
+  return (
+    <div className="app">
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Routes>
+
+          <Route path="/" element={<LoginPage />} />
+          <Route path="/home" element={isAuth ? <HomePage /> : <Navigate to="/" />} />
+          <Route path="/profile/:userId" element={isAuth ? <ProfilePage /> : <Navigate to="/" />}
+          />
+
+        </Routes>
+      </ThemeProvider>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/config.js
@@ -0,0 +1,6 @@
+
+// Local_Host
+// export const API = "http://localhost:4000";
+
+// Deploy
+ export const API = "https://sm-backend-seven.vercel.app"; 
